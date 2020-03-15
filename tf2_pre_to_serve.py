ee714f63d88fb7ecf0e3532101b65363fb232c52
lordtt13 <thakurtanmay72@yahoo.com>
2020-03-15 15:02:21 +0530

    Added Model
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- tf2_pre_to_serve.py
+++ tf2_pre_to_serve.py
@@ -25,3 +25,44 @@ print(x_train.shape)
 # number of classes
 K = len(set(y_train))
 print("number of classes:", K)
+
+"""
+# Model Definition using Sequential Class
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(32, (3, 3), strides=2, activation='relu', input_shape = (x_train.shape)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), strides=2, activation='relu'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Conv2D(128, (3, 3), strides=2, activation='relu'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(1024, activation = 'relu'))
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.Dense(K, activation = 'softmax'))
+"""
+
+# Build the model using the functional API
+i = tf.keras.layers.Input(shape=x_train[0].shape)
+x = tf.keras.layers.SeparableConv2D(32, (3, 3), activation='relu', padding='same')(i)
+x = tf.keras.layers.BatchNormalization()(x)
+x = tf.keras.layers.SeparableConv2D(64, (3, 3), activation='relu', padding='same')(x)
+x = tf.keras.layers.BatchNormalization()(x)
+x = tf.keras.layers.MaxPooling2D((2, 2))(x)
+x = tf.keras.layers.SeparableConv2D(128, (3, 3), activation='relu', padding='same')(x)
+x = tf.keras.layers.BatchNormalization()(x)
+x = tf.keras.layers.MaxPooling2D((2, 2))(x)
+x = tf.keras.layers.GlobalMaxPooling2D()(x)
+x = tf.keras.layers.Dense(1024, activation='relu')(x)
+x = tf.keras.layers.Dropout(0.2)(x)
+x = tf.keras.layers.Dense(K, activation='softmax')(x)
+
+model = tf.keras.models.Model(i, x)
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, validation_data = (x_test, y_test), epochs = 10)
